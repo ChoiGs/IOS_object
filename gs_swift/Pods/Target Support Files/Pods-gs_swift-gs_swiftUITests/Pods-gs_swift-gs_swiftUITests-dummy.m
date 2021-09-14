b498cebd7aeaf93bfc82f257ff8f757815ccf31e
gwangseon <choi@gwangseons-MacBook-Pro.local>
2021-09-14 10:47:57 +0900

    swift 프로젝트 생성

---
+++ gs_swift/Pods/Target Support Files/Pods-gs_swift-gs_swiftUITests/Pods-gs_swift-gs_swiftUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_gs_swift_gs_swiftUITests : NSObject
+@end
+@implementation PodsDummy_Pods_gs_swift_gs_swiftUITests
+@end
---
+++ gs_swift/Pods/Target Support Files/Pods-gs_swiftTests/Pods-gs_swiftTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_gs_swiftTests : NSObject
+@end
+@implementation PodsDummy_Pods_gs_swiftTests
+@end
